080f8ee019607eb49b1b827cbcb6db7ccc4e7d08
Muiz_Git_SourceTree <muiznaveedrana@gmail.com>
2024-08-18 17:08:45 +0100

    Make fr

--- backend.py
+++ backend.py
@@ -2,12 +2,19 @@ import requests
 
 API_Key = "28ea11e54b5618c5d9c8cc20a758de96"
 
-def get_data(place, days = 1, kind = "temperature"):
+def get_data(place, days = 1, kind = "Temperature"):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_Key}"
     response = requests.get(url)
     data = response.json()
-    print(data)
-    return data
+    filtered_data = data["list"]
+    nr_values = 8 * days
+    filtered_data = filtered_data[:nr_values]
+
+    if kind == "Temperature":
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+    else:
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+    return filtered_data
 
 if __name__ == "__main__":
-    get_data("Tokyo")
+    print(get_data("Tokyo"))
